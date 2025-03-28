110ec0f6811156b9f0d01fe35868415083f9108d
Damian Matuszak <damian_matuszak@o2.pl>
2025-03-28 19:08:58 +0100

    add repoLink

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,7 @@ const modal = document.getElementById("portfolioModal");
 const modalTitle = document.getElementById("modal-title");
 const modalDescription = document.getElementById("modal-description");
 const modalLink = document.getElementById("modal-link");
+const modalRepoLink = document.getElementById("modal-repo-link");
 const closeBtn = document.querySelector(".close");
 
 // Pobranie wszystkich elementów portfolio
@@ -18,27 +19,33 @@ const portfolioItems = document.querySelectorAll(".portfolio-item .portfolio-pic
 
 const portfolioDetails = {
     "Data Managment CRUD App": {
-        link: "#",
+        link: "https://project-data-managment-crud-app.netlify.app/",
+        repoLink: 'https://github.com/Damian080990/crud-data-app',
         description: "A CRUD application with data printing functionality, developed with React.js and Tailwind CSS."
     },
     "Guess the number game": {
         link: "https://damian080990.github.io/guess-a-number-game-js/",
+        repoLink: 'https://github.com/Damian080990/guess-a-number-game-js',
         description: "A game in which the player tries to guess a randomly selected number within a defined range."
     },
     "Simple Landing Page": {
         link: "https://damian080990.github.io/real-estate-landing-page/",
+        repoLink: 'https://github.com/Damian080990/real-estate-landing-page',
         description: "A minimalistic landing page built with HTML/CSS."
     },
     "Sociala": {
         link: "https://project-sociala.netlify.app/",
+        repoLink: 'https://github.com/Damian080990/react-project-sociala',
         description: "A social media application inspired by Facebook/Instagram, featuring user post feeds. Developed using React.js."
     },
     "Simple Calculator": {
         link: "https://project-simple-react-calculator.netlify.app/",
+        repoLink: 'https://github.com/Damian080990/react-calculator',
         description: "React.js-based calculator application implementing core arithmetic operations."
     },
     "CV Template": {
         link: "https://damian080990.github.io/cv/",
+        repoLink: 'https://github.com/Damian080990/cv',
         description: "Customizable resume template developed using HTML5, CSS3, and vanilla JavaScript."
     },
 }
@@ -53,6 +60,7 @@ portfolioItems.forEach(item => {
         modalTitle.innerText = title;
         modalDescription.innerText = portfolioData.description;
         modalLink.href = portfolioData.link;
+        modalRepoLink.href = portfolioData.repoLink; // Ustawienie linku do repozytorium
 
         // Pokaż modal
         modal.style.display = "flex";
